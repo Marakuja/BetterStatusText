5757fe27aa0d4d3030f57bffc0296893e5a2ed20
Marakuja <andreas.muerell@gmail.com>
2017-01-20 10:45:00 +0100

    removed unnecessary code, added NONE functionality

--- BetterStatusText/BetterStatusText.lua
+++ BetterStatusText/BetterStatusText.lua
@@ -3,25 +3,25 @@
 
 hooksecurefunc("TextStatusBar_UpdateTextStringWithValues", function(statusFrame, textString, value, valueMin, valueMax)
 
-	if( statusFrame.LeftText and statusFrame.RightText ) then
-		statusFrame.LeftText:SetText("");
-		statusFrame.RightText:SetText("");
-		statusFrame.LeftText:Hide();
-		statusFrame.RightText:Hide();
-	end
+	-- if( statusFrame.LeftText and statusFrame.RightText ) then
+	-- 	statusFrame.LeftText:SetText("");
+	-- 	statusFrame.RightText:SetText("");
+	-- 	statusFrame.LeftText:Hide();
+	-- 	statusFrame.RightText:Hide();
+	-- end
 
 	if ( ( tonumber(valueMax) ~= valueMax or valueMax > 0 ) and not ( statusFrame.pauseUpdates ) ) then
-		statusFrame:Show();
+		-- statusFrame:Show();
 		
-		if ( (statusFrame.cvar and GetCVar(statusFrame.cvar) == "1" and statusFrame.textLockable) or statusFrame.forceShow ) then
-			textString:Show();
-		elseif ( statusFrame.lockShow > 0 and (not statusFrame.forceHideText) ) then
-			textString:Show();
-		else
-			textString:SetText("");
-			textString:Hide();
-			return;
-		end
+		-- if ( (statusFrame.cvar and GetCVar(statusFrame.cvar) == "1" and statusFrame.textLockable) or statusFrame.forceShow ) then
+		-- 	textString:Show();
+		-- elseif ( statusFrame.lockShow > 0 and (not statusFrame.forceHideText) ) then
+		-- 	textString:Show();
+		-- else
+		-- 	textString:SetText("");
+		-- 	textString:Hide();
+		-- 	return;
+		-- end
 
 		local valueDisplay = value;
 		local valueMaxDisplay = valueMax;
@@ -33,6 +33,7 @@ hooksecurefunc("TextStatusBar_UpdateTextStringWithValues", function(statusFrame,
 			valueMaxDisplay = BreakUpLargeNumbers(valueMax);
 		end
 
+		local mytext = "";
 		local textDisplay = GetCVar("statusTextDisplay");
 		if ( value and valueMax > 0 and ( textDisplay ~= "NUMERIC" or statusFrame.showPercentage ) and not statusFrame.showNumeric) then
 			if ( value == 0 and statusFrame.zeroText ) then
@@ -40,54 +41,31 @@ hooksecurefunc("TextStatusBar_UpdateTextStringWithValues", function(statusFrame,
 				statusFrame.isZero = 1;
 				textString:Show();
 			elseif ( textDisplay == "BOTH" and not statusFrame.showPercentage) then
+				-- BOTH
                 -- here begins our addition, mytext for storing information
-				local mytext = "";
 				if( statusFrame.LeftText and statusFrame.RightText ) then
 					if(not statusFrame.powerToken or statusFrame.powerToken == "MANA") then
 						mytext = math.ceil((value / valueMax) * 100) .. "% " -- only show the percentage when showing Mana
-						statusFrame.LeftText:SetText(math.ceil((value / valueMax) * 100) .. "%");
+						-- statusFrame.LeftText:SetText(math.ceil((value / valueMax) * 100) .. "%");
 						statusFrame.LeftText:Hide(); -- hide LeftText string
 					end
-					statusFrame.RightText:SetText(valueDisplay);
+					-- statusFrame.RightText:SetText(valueDisplay);
 					statusFrame.RightText:Hide(); -- hide RightText string
 					textString:Show(); -- now show the middle textString
 				else
 					valueDisplay = "(" .. math.ceil((value / valueMax) * 100) .. "%) " .. valueDisplay .. " / " .. valueMaxDisplay;
 				end
-				-- begin
-				-- textString:Show();
-				-- statusFrame.LeftText:Hide();
-				-- statusFrame.RightText:Hide();
-				-- end
 				textString:SetText(mytext .. valueDisplay); -- set the textstring to our generated values
 			else
-				valueDisplay = math.ceil((value / valueMax) * 100) .. "%";
-				if ( statusFrame.prefix and (statusFrame.alwaysPrefix or not (statusFrame.cvar and GetCVar(statusFrame.cvar) == "1" and statusFrame.textLockable) ) ) then
-					textString:SetText(statusFrame.prefix .. " " .. valueDisplay);
-				else
-					textString:SetText(valueDisplay);
+				--NONE -> no % please
+				if(not statusFrame.powerToken or statusFrame.powerToken == "MANA") then
+					mytext = math.ceil((value / valueMax) * 100) .. "% " -- only show the percentage when showing Mana
+					-- statusFrame.LeftText:SetText(math.ceil((value / valueMax) * 100) .. "%");
 				end
+				textString:SetText(mytext .. valueDisplay);
 			end
-		elseif ( value == 0 and statusFrame.zeroText ) then
-			textString:SetText(statusFrame.zeroText);
-			statusFrame.isZero = 1;
-			textString:Show();
-			return;
 		else
-			statusFrame.isZero = nil;
-			if ( statusFrame.prefix and (statusFrame.alwaysPrefix or not (statusFrame.cvar and GetCVar(statusFrame.cvar) == "1" and statusFrame.textLockable) ) ) then
-				textString:SetText(statusFrame.prefix.." "..valueDisplay.." / "..valueMaxDisplay);
-			else
-				textString:SetText(valueDisplay.." / "..valueMaxDisplay);
-			end
-		end
-	else
-		textString:Hide();
-		textString:SetText("");
-		if ( not statusFrame.alwaysShow ) then
-			statusFrame:Hide();
-		else
-			statusFrame:SetValue(0);
+			--NUMERIC
 		end
 	end
 end)
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A Small WoW Addon
 
 I like the standard UI in World of Warcraft, but the new Status Text Display with both Values on the left and right side bothers me.
 
-So I wrote this small Addon that adds a hook to the update function from the standard interface to deal with this but also gives all the information we need.
+So I wrote this small Addon that adds a hook to the update function from the standard interface to deal with this but also gives all the information we need. Now works also with option "NONE" when hovering over the statusbars with the mouse.
 
 Pictures:
 Settings must be set to "both" to see the new string:
